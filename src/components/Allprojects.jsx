001f3fae58e29f5ffba0fc31a3486f765426f56d
edmund12-oss <61464445+edmund12-oss@users.noreply.github.com>
2025-04-15 00:54:07 -0700

    Allprojects.jsx

--- src/components/Allprojects.jsx
+++ src/components/Allprojects.jsx
@@ -244,7 +244,7 @@ const Allprojects = () => {
                 <div className="footer-contact">
                     <p>Ambassador house</p>
                     <p>Along Kampala road</p>
-                    <p className="mt-3"><strong>Phone:</strong> <span>+256 701 601 604</span></p>
+                    <p className="mt-3"><strong>Phone:</strong> <span>+256 767 655 978</span></p>
                     <p><strong>Email:</strong> <span>terrain_consult@yahoo.com</span></p>
                 </div>
             </div>
